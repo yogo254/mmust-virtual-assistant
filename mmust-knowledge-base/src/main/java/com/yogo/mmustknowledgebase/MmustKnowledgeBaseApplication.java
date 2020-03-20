94ca1c495fc05cfc0a3157ef6670910adeecffb5
yogo254 <yogo254@yahoo.com>
2020-03-20 05:22:32 +0300

    adding the knowledgebase service

---
+++ mmust-knowledge-base/src/main/java/com/yogo/mmustknowledgebase/MmustKnowledgeBaseApplication.java
@@ -0,0 +1,13 @@
+package com.yogo.mmustknowledgebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmustKnowledgeBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmustKnowledgeBaseApplication.class, args);
+	}
+
+}
---
+++ mmust-knowledge-base/src/test/java/com/yogo/mmustknowledgebase/MmustKnowledgeBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yogo.mmustknowledgebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmustKnowledgeBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
